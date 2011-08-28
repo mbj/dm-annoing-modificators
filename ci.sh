5232f124461f1dae249b79e6065aad561046087e
Markus Schirp <mbj@seonic.net>
2011-08-28 18:10:44 +0200

    Fixed ci

--- ci.sh
+++ ci.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 source ~/.profile
 rvm use ruby-1.9.2
-rvm gemset create dm-annoing-modificators
-rvm gemset use dm-annoing-modificators
 gem install bundler
 bundle install
 bundle exec rspec spec
-#bundle exec cucumber features
